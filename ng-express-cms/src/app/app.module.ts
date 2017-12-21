69e8b24c7a1016366f43d246c89f312c53d5382c
Vandivier <vandivier_john@yahoo.com>
2017-12-20 21:58:17 -0500

    2 way bind title

--- ng-express-cms/src/app/app.module.ts
+++ ng-express-cms/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -22,7 +23,8 @@ import { SignInComponent } from './sign-in/sign-in.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ng-express-cms/src/app/dashboard/dashboard.component.ts
+++ ng-express-cms/src/app/dashboard/dashboard.component.ts
@@ -9,6 +9,7 @@ export class DashboardComponent implements OnInit {
 
   itemCount: number = 4;
   sSubmitArticleButtonText: string = 'Publish'; // TODO: or Schedule or Update depending on WordPress-like conditions
+  sArticleTitle: string = 'This is my first article!';
 
   constructor() { }
 
