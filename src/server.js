bdf357d05adb11daeb79bbc370f4f26081f16e89
John Vandivier <john.vandivier@halfaker.com>
2018-01-03 11:54:16 -0500

    connect via test user

--- src/server.js
+++ src/server.js
@@ -33,8 +33,8 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "password"
+  user: "test",
+  password: "test"
 });
 
 con.connect(function(err) {
